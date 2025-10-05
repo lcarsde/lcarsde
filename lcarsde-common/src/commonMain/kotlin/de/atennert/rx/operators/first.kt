7d4d8ffc9afff7f924b800c4b879a67146589c83
Andreas Tennert <mail@andreas-tennert.de>
2025-10-05 19:51:29 +0200

    refactor: move rx to common

--- lcarsde-common/src/commonMain/kotlin/de/atennert/rx/operators/first.kt
+++ lcarsde-common/src/commonMain/kotlin/de/atennert/rx/operators/first.kt
@@ -5,16 +5,13 @@ import de.atennert.rx.Observer
 import de.atennert.rx.Operator
 import de.atennert.rx.Subscription
 
-fun <A> last() = Operator<A, A> { source ->
+fun <A> first() = Operator { source ->
     Observable { subscriber ->
-        var lastValue: A? = null
-        var initialized = false
-
         val subscription = Subscription()
         subscription.add(source.subscribe(object : Observer<A> {
             override fun next(value: A) {
-                lastValue = value
-                initialized = true
+                subscriber.next(value)
+                subscriber.complete()
             }
 
             override fun error(error: Throwable) {
@@ -22,10 +19,6 @@ fun <A> last() = Operator<A, A> { source ->
             }
 
             override fun complete() {
-                if (initialized) {
-                    @Suppress("UNCHECKED_CAST")
-                    subscriber.next(lastValue as A)
-                }
                 subscriber.complete()
             }
         }))
